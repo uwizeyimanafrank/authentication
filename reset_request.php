41969e3a162578f820a0711098e61e78d1bc7aca
uwizeyimanafrank <frankuwizeyimana2@gmail.com>
2025-04-22 12:00:46 +0200

    Update reset_request.php

--- reset_request.php
+++ reset_request.php
@@ -1,10 +1,1 @@
-<?php
-session_start();
-?>
 
-<form method="POST" action="process_reset.php">
-    <h2>Reset Password</h2>
-    <input type="text" name="username" required placeholder="Username"><br>
-    <input type="email" name="email" required placeholder="Email"><br>
-    <button type="submit">Request Reset Code</button>
-</form>
